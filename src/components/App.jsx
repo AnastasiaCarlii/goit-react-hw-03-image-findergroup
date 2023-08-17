314451723c38b5370cb6aa2214fe3103229eac96
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-17 14:47:12 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+  };
+
+  changeQuery = newQuery => {
+    this.setState({
+      query: newQuery,
+    });
+  };
+  setImages = () => {};
+
+  render() {
+    return (
+      <div>
+        <div>
+          <form
+            onSubmit={evt => {
+              evt.preventDefault();
+
+              this.changeQuery(evt.target.elements.query.value);
+              evt.target.reset();
+            }}
+          >
+            <input type="text" name="query" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div>Gallery</div>
+        <div>
+          <button>Load more</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
